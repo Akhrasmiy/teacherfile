9e69aa131f9bf9905da8edb9065fec6f38b5c777
Jahongir <akhrasmiy@gmail.com>
2023-06-09 18:05:11 +0500

    origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/project', {
   useUnifiedTopology: true,
 });
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(userRout);
 app.use(fileRout);
 app.use(teacherRout)
